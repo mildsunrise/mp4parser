abf3306f797ae29c6d308be37c00f49992f7429e
Alba Mendez <me@alba.sh>
2024-04-23 13:15:19 +0200

    DRM support
    we can't easily parse senc as it requires info from
    other boxes... and I also left seig unparsed for now

--- mp4parser.py
+++ mp4parser.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 '''
-Code by Alba Mendez, manually copied and pasted, had 8 revisions when copied.
-https://gist.github.com/mildsunrise/ffd74730504e4dc44f47fc7528e7bf59
-
 Portable* ISO Base Media File Format dissector / parser.
 Usage: ./mp4parser.py <file name>
 
@@ -162,6 +159,8 @@ def print_hex_dump(data: memoryview, prefix: str):
 
 def print_error(exc, prefix: str):
 	print(prefix + f'{ansi_bold(ansi_fg1("ERROR:"))} {ansi_fg1(exc)}\n')
+
+format_uuid = lambda x: '-'.join(x.hex()[s:e] for s, e in itertools.pairwise([0, 8, 12, 16, 20, 32]) )
 
 
 # CORE PARSING
@@ -425,6 +424,17 @@ box_registry = [
 		'mvra': ('MultiviewRelationAttributeBox', 'FullBox'),
 	}),
 	({
+		'name': 'ISO/IEC 23001-7',
+		'title': 'Information technology — MPEG systems technologies — Part 7: Common encryption in ISO base media file format files',
+		'version': '2016',
+		'url': 'https://www.iso.org/standard/68042.html',
+	}, {
+		'seig': ('CencSampleEncryptionInformationGroupEntry', 'SampleGroupEntry'),
+		'senc': ('SampleEncryptionBox', 'FullBox'),
+		'pssh': ('ProtectionSystemSpecificHeaderBox', 'FullBox'),
+		'tenc': ('TrackEncryptionBox', 'FullBox'),
+	}),
+	({
 		'name': 'ISO/IEC 23008-12',
 		'title': 'Information technology — MPEG systems technologies — Part 12: Image File Format',
 		'version': '17 January 2014 working draft',
@@ -489,7 +499,7 @@ def slice_box(mem: memoryview):
 
 	if btype == 'uuid':
 		assert len(mem) >= pos + 16, f'EOF while reading UUID'
-		btype = mem[pos:pos + 16].hex()
+		btype = mem[pos:pos + 16]
 		pos += 16
 
 	assert length >= pos, f'invalid {btype} length: {length}'
@@ -514,14 +524,13 @@ def parse_boxes(offset: int, mem: memoryview, indent=0, contents_fn=None):
 			offset_text = ' (large size)' + offset_text
 		type_label = btype
 		if len(btype) != 4: # it's a UUID
-			type_label = '-'.join(btype[s:e] for s, e in itertools.pairwise([0, 8, 12, 16, 20, 32]) )
-			type_label = f'UUID {type_label}'
+			type_label = f'UUID {format_uuid(btype)}'
 		print(prefix + ansi_bold(f'[{type_label}]') + name_text + offset_text + length_text)
 		result.append( (contents_fn or parse_contents)(btype, offset + length - len(data), data, indent + 1) )
 		mem, offset = mem[length:], offset + length
 	return result
 
-nesting_boxes = { 'moov', 'trak', 'mdia', 'minf', 'dinf', 'stbl', 'mvex', 'moof', 'traf', 'mfra', 'meco', 'edts', 'udta' }
+nesting_boxes = { 'moov', 'trak', 'mdia', 'minf', 'dinf', 'stbl', 'mvex', 'moof', 'traf', 'mfra', 'meco', 'edts', 'udta', 'sinf', 'schi' }
 # apple(?) / quicktime metadata
 nesting_boxes |= { 'ilst', '\u00a9too', '\u00a9nam', '\u00a9day', '\u00a9ART', 'aART', '\u00a9alb', '\u00a9cmt', '\u00a9day', 'trkn', 'covr', '----' }
 
@@ -1495,6 +1504,125 @@ def parse_trun_box(offset: int, data: memoryview, indent: int):
 	assert not left, f'{len(left)} bytes of trailing data'
 
 # FIXME: describe handlers, boxes (from RA, also look at the 'handlers' and 'unlisted' pages), brands
+
+
+# DRM boxes
+
+def parse_frma_box(offset: int, data: memoryview, indent: int):
+	prefix = ' ' * (indent * indent_n)
+	data = io.BytesIO(data)
+
+	data_format, = unpack(data, '4s')
+	data_format = data_format.decode('latin1')
+	print(prefix + f'data_format = {repr(data_format)}')
+
+	left = data.read()
+	assert not left, f'{len(left)} bytes of trailing data'
+
+def parse_schm_box(offset: int, data: memoryview, indent: int):
+	prefix = ' ' * (indent * indent_n)
+	data = io.BytesIO(data)
+	version, box_flags = parse_fullbox(data, prefix)
+	assert version == 0, f'invalid version: {version}'
+	print(prefix + f'version = {version}, flags = {box_flags:06x}')
+
+	scheme_type, scheme_version = unpack(data, '4sI')
+	scheme_type = scheme_type.decode('latin1')
+	print(prefix + f'scheme_type = {repr(scheme_type)}')
+	print(prefix + f'scheme_version = {scheme_version:#x}')
+	if box_flags & 1:
+		scheme_uri = read_string(data)
+		print(prefix + f'scheme_uri = {repr(scheme_uri)}')
+
+	left = data.read()
+	assert not left, f'{len(left)} bytes of trailing data'
+
+def parse_tenc_box(offset: int, data: memoryview, indent: int):
+	prefix = ' ' * (indent * indent_n)
+	data = io.BytesIO(data)
+	version, box_flags = parse_fullbox(data, prefix)
+	print(prefix + f'version = {version}, flags = {box_flags:06x}')
+
+	reserved_1, reserved_2 = unpack(data, 'BB')
+	assert reserved_1 == 0, f'invalid reserved 1: {reserved_1}'
+	if version > 0:
+		default_crypt_byte_block, default_skip_byte_block = split_bits(reserved_2, 8, 4, 0)
+		print(prefix + f'default_crypt_byte_block = {default_crypt_byte_block}')
+		print(prefix + f'default_skip_byte_block = {default_skip_byte_block}')
+	else:
+		assert reserved_2 == 0, f'invalid reserved 2: {reserved_2}'
+
+	default_isProtected, default_Per_Sample_IV_Size, default_KID = unpack(data, 'BB16s')
+	print(prefix + f'default_isProtected = {default_isProtected}')
+	print(prefix + f'default_Per_Sample_IV_Size = {default_Per_Sample_IV_Size}')
+	print(prefix + f'default_KID = {default_KID.hex()}')
+	if default_isProtected == 1 and default_Per_Sample_IV_Size == 0:
+		default_constant_IV_size, = unpack(data, 'B')
+		default_constant_IV = data.read(default_constant_IV_size)
+		assert len(default_constant_IV) == default_constant_IV_size, \
+			f'unexpected EOF within default_constant_IV: expected {default_constant_IV_size}, got {len(default_constant_IV)}'
+		print(prefix + f'default_constant_IV = {default_constant_IV.hex()}')
+
+	left = data.read()
+	assert not left, f'{len(left)} bytes of trailing data'
+
+def parse_pssh_box(offset: int, data: memoryview, indent: int):
+	prefix = ' ' * (indent * indent_n)
+	data = io.BytesIO(data)
+	version, box_flags = parse_fullbox(data, prefix)
+	print(prefix + f'version = {version}, flags = {box_flags:06x}')
+
+	SystemID, = unpack(data, '16s')
+	system_name, system_comment = protection_systems.get(format_uuid(SystemID), (None, None))
+	system_desc = f' ({system_name})' if system_name else ''
+	print(prefix + f'SystemID = {format_uuid(SystemID)}{system_desc}')
+
+	if version > 0:
+		KID_count, = unpack(data, 'I')
+		for i in range(KID_count):
+			KID, = unpack(data, '16s')
+			print(prefix + f'- KID: {KID.hex()}')
+
+	DataSize, = unpack(data, 'I')
+	Data = data.read(DataSize)
+	assert len(Data) == DataSize, f'unexpected EOF within Data: expected {DataSize}, got {len(Data)}'
+	print(prefix + f'Data =')
+	print_hex_dump(Data, prefix + '  ')
+
+	left = data.read()
+	assert not left, f'{len(left)} bytes of trailing data'
+
+# https://dashif.org/identifiers/content_protection#protection-system-specific-identifiers
+protection_systems = {
+	'6dd8b3c3-45f4-4a68-bf3a-64168d01a4a6': ('ABV DRM (MoDRM)', 'For further information please contact ABV International Pte Ltd. Documentation is available under NDA. ABV Content Protection for MPEG DASH (MoDRM v4.7 and above).'),
+	'f239e769-efa3-4850-9c16-a903c6932efb': ('Adobe Primetime DRM version 4', 'For further information please contact Adobe.'),
+	'616c7469-6361-7374-2d50-726f74656374': ('Alticast', 'For further information please contact Alticast. galtiprotect_drm@alticast.com.'),
+	'94ce86fb-07ff-4f43-adb8-93d2fa968ca2': ('Apple FairPlay', 'Content Protection System Identifier for Apple FairPlay Streaming.'),
+	'279fe473-512c-48fe-ade8-d176fee6b40f': ('Arris Titanium', 'For further information please contact multitrust.info@arris.com. Documentation is available under NDA. @value is specified in documentation related to a specific version of the product.'),
+	'3d5e6d35-9b9a-41e8-b843-dd3c6e72c42c': ('ChinaDRM', 'ChinaDRM is defined by China Radio and Television Film Industry Standard GY/T 277-2014. @value indicates ChinaDRM specific solution provided by various vendors.'),
+	'3ea8778f-7742-4bf9-b18b-e834b2acbd47': ('Clear Key AES-128', 'Identifier for HLS Clear Key encryption using CBC mode. This is to be used as an identifier when requesting key system information when using CPIX.'),
+	'be58615b-19c4-4684-88b3-c8c57e99e957': ('Clear Key SAMPLE-AES', 'Identifier for HLS Clear Key encryption using CBCS mode. This is to be used as an identifier when requesting key system information when using CPIX.'),
+	'e2719d58-a985-b3c9-781a-b030af78d30e': ('Clear Key DASH-IF', 'This identifier is meant to be used to signal the availability of W3C Clear Key in the context of a DASH presentation.'),
+	'644fe7b5-260f-4fad-949a-0762ffb054B4': ('CMLA (OMA DRM)', 'A draft version of the CMLA Technical Specification which is in process with involved adopters is not published. It is planned to be chapter 18 of our CMLA Technical Specification upon completion and approval.Revisions of the CMLA Technical Specification become public upon CMLA approval. UUID will correlate to various related XML schema and PSSH components as well as elements of the content protection element relating to CMLA DASH mapping.'),
+	'37c33258-7b99-4c7e-b15d-19af74482154': ('Commscope Titanium V3', 'Documentation available under NDA. @value is specified in documentation related to a specific version of the product. Contact multitrust.info@arris.com for further information.'),
+	'45d481cb-8fe0-49c0-ada9-ab2d2455b2f2': ('CoreCrypt', 'CoreTrust Content Protection for MPEG-DASH. For further information and specification please contact CoreTurst at mktall@coretrust.com.'),
+	'dcf4e3e3-62f1-5818-7ba6-0a6fe33ff3dd': ('DigiCAP SmartXess', 'For further information please contact DigiCAP. Documentation is available under NDA. DigiCAP SmartXess for DASH @value CA/DRM_NAME VERSION (CA 1.0, DRM+ 2.0)'),
+	'35bf197b-530e-42d7-8b65-1b4bf415070f': ('DivX DRM Series 5', 'Please contact DivX for specifications.'),
+	'80a6be7e-1448-4c37-9e70-d5aebe04c8d2': ('Irdeto Content Protection', 'For further information please contact Irdeto. Documentation is available under NDA.'),
+	'5e629af5-38da-4063-8977-97ffbd9902d4': ('Marlin Adaptive Streaming Simple Profile V1.0', 'Details of what can be further specified within the ContentProtection element is in the specifications.'),
+	'9a04f079-9840-4286-ab92-e65be0885f95': ('Microsoft PlayReady', 'For further information please contact Microsoft.'),
+	'6a99532d-869f-5922-9a91-113ab7b1e2f3': ('MobiTV DRM', 'Identifier for any version of MobiDRM (MobiTV DRM). The version is signaled in the pssh box.'),
+	'adb41c24-2dbf-4a6d-958b-4457c0d27b95': ('Nagra MediaAccess PRM 3.0', 'It identifies Nagra MediaAccess PRM 3.0 and above. Documentation is available under NDA.'),
+	'1f83e1e8-6ee9-4f0d-ba2f-5ec4e3ed1a66': ('SecureMedia', 'Documentation is available under NDA. @value shall be Arris SecureMedia version XXXXXXX. XXXXXX is specified in documentation associated with a particular version of the product. The UUID will be made available in SecureMedia documentation shared with a partner or customer of SecureMedia Arris.'),
+	'992c46e6-c437-4899-b6a0-50fa91ad0e39': ('SecureMedia SteelKnot', 'Documentation is available under NDA. @value shall be Arris SecureMedia SteelKnot version XXXXXXX. The exact length and syntax of XXXXXXX is specified in documentation associated with a particular version of the product. The UUID will be made available in SecureMedia SteelKnot documentation shared with a partner or customer of SecureMedia SteelKnot.'),
+	'a68129d3-575b-4f1a-9cba-3223846cf7c3': ('Synamedia/Cisco/NDS VideoGuard DRM', 'Documentation is available under NDA.'),
+	'aa11967f-cc01-4a4a-8e99-c5d3dddfea2d': ('Unitend DRM (UDRM)', 'For further information please contact y.ren@unitend.com.'),
+	'9a27dd82-fde2-4725-8cbc-4234aa06ec09': ('Verimatrix VCAS', '@value is Verimatrix VCAS for DASH ViewRightWeb VV.vv (VV.vv is the version number). If used this can help the client to determine if the current DRM client can play the content.'),
+	'b4413586-c58c-ffb0-94a5-d4896c1af6c3': ('Viaccess-Orca DRM (VODRM)', 'For further information please contact Viaccess-Orca. VODRM documentation is available under NDA.'),
+	'793b7956-9f94-4946-a942-23e7ef7e44b4': ('VisionCrypt', 'For further information please contact gosdrm@gospell.com.'),
+	'1077efec-c0b2-4d02-ace3-3c1e52e2fb4b': ('W3C Common PSSH box', 'This identifier is to be used as the SystemID for the Common PSSH box format defined by W3C as a preferred alternative to DRM system specific PSSH box formats. This identifier may be used in PSSH boxes and MPEG-DASH ContentProtection elements.'),
+	'edef8ba9-79d6-4ace-a3c8-27dcd51d21ed': ('Widevine Content Protection', 'For further information please contact Widevine.'),
+}
 
 
 # MPEG-4 part 1 DESCRIPTORS (based on 2010 edition)
