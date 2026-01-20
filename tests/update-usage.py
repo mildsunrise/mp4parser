6097137e44e5037891a1e8392a8e058406904592
Alicia Boya García <aboya@igalia.com>
2026-01-20 17:07:53 +0100

    Use tabs consistently
    Replace usage of spaces in code with tabs.

--- tests/update-usage.py
+++ tests/update-usage.py
@@ -9,17 +9,17 @@ os.chdir('..')
 
 # we could also import options.py and do parser.format_help(), but I like the wrapping...
 helpstr = run(["./mp4parser.py", "--help"],
-    check=True, capture_output=True, encoding="utf-8").stdout
+	check=True, capture_output=True, encoding="utf-8").stdout
 
 helpstr = f'```\n{helpstr}```\n'
 
 with open('README.md', 'r+') as f:
-    text = list(f)
-    idx1 = text.index('<!-- BEGIN USAGE -->\n')
-    idx2 = text.index('<!-- END USAGE -->\n')
-    assert idx1 < idx2
-    text = text[:idx1+1] + [helpstr] + text[idx2:]
-    f.seek(0)
-    f.truncate(0)
-    f.write(''.join(text))
+	text = list(f)
+	idx1 = text.index('<!-- BEGIN USAGE -->\n')
+	idx2 = text.index('<!-- END USAGE -->\n')
+	assert idx1 < idx2
+	text = text[:idx1+1] + [helpstr] + text[idx2:]
+	f.seek(0)
+	f.truncate(0)
+	f.write(''.join(text))
 
